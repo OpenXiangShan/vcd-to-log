8e3753639500ecbee26cc75448f0c95b9d5d8cfb
happy-lx <1037997956@qq.com>
2021-04-16 13:10:54 +0800

    feat:implement some features * +  -  *  ||  ==  != * !  ~  &  | * Constants

--- src/XVisitor.cpp
+++ src/XVisitor.cpp
@@ -32,32 +32,42 @@ antlrcpp::Any XVisitor::visitExpression(XiangParser::ExpressionContext *ctx) {
             // this is a signal
             return visitSignal(dynamic_cast<XiangParser::SignalContext *>(ctx->children.front()));
         }
-    } else {
-        // for now, we don't support unary op
-        assert(ctx->children.size() == 3);
+    } else if(ctx->children.size() == 3){
         auto *lexp = dynamic_cast<XiangParser::ExpressionContext *>(ctx->children[0]);
         auto *rexp = dynamic_cast<XiangParser::ExpressionContext *>(ctx->children[2]);
         VCDValue* lv = visitExpression(lexp);
         VCDValue* rv = visitExpression(rexp);
         VCDValue* res;
-
+        
         switch (ctx->op->getType()) {
             case XiangParser::Mul:
+                res = (*lv) * (*rv);
                 break;
             case XiangParser::Div:
                 break;
             case XiangParser::Add:
+                res = *lv + *rv;
                 break;
             case XiangParser::Minus:
+                res = *lv - *rv;
                 break;
             case XiangParser::Equal:
+                res = *lv == *rv;
                 break;
             case XiangParser::NotEqual:
+                res = *lv != *rv;
                 break;
             case XiangParser::And:
                 res = *lv && *rv;
                 break;
             case XiangParser::Or:
+                res = *lv || *rv;
+                break;
+            case XiangParser::BitOr:
+                res = *lv | *rv;
+                break;
+            case XiangParser::BitAnd:
+                res = *lv & *rv;
                 break;
             default:
                 assert(false && "Unknown Op type");
@@ -66,15 +76,215 @@ antlrcpp::Any XVisitor::visitExpression(XiangParser::ExpressionContext *ctx) {
             tmp_results.push_back(res);
         }
         return res;
+    } else
+    {
+        // TODO, support unary op
+        assert(ctx->children.size() == 2);
+        //for now , we just support '!' and '~' operator
+        auto *pre       = ctx->op;
+        auto *exp_child = dynamic_cast<XiangParser::ExpressionContext*>(ctx->children[1]);
+        VCDValue* exp_res = visitExpression(exp_child);
+        if(pre->getType() == XiangParser::Not)
+        {
+            if(exp_res->asBool())
+            {
+                return new VCDValue(VCD_0);
+            }else
+            {
+                return new VCDValue(VCD_1);
+            }
+        }else if(pre->getType() == XiangParser::Reverse)
+        {
+            if(exp_res->get_type() == VCD_SCALAR)
+            {
+                if(exp_res->asBool())
+                {
+                    return new VCDValue(VCD_0);
+                }else
+                {
+                    return new VCDValue(VCD_1);
+                }
+                
+            }else
+            {
+                return new VCDValue(VCDValue::Reverse(exp_res->get_value_vector()));
+            }
+            
+        }else
+        {
+            assert(false && "unimplememted unary op");
+        }
+        
+        
+
     }
+    
 }
+
+const static std::vector<std::string> HexMap = {
+    "0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"
+    };
+const static std::vector<std::string> OctMap = {
+    "000","001","010","011","100","101","110","111"
+    };
 
 antlrcpp::Any XVisitor::visitSignal(XiangParser::SignalContext *ctx) {
     StrPath signalPath = ctx->getText();
-    VCDSignal* sig = trace->get_signal_by_path(signalPath);
-    assert(sig && "Signal can't be found!");
-    VCDValue* v = trace->get_signal_value_at(sig->hash, time);
-    return v;
+    // std::cout << signalPath << std::endl;
+    //handle constants
+    if(signalPath[0] == '0' && signalPath[1] == 'x')
+    {
+        VCDValue* v = new VCDValue(new VCDBitVector());
+        for(int i=2;i<signalPath.length();i++)
+        {
+            if(signalPath[i] >= '0' && signalPath[i] <= '9')
+            {
+                //0x0123456789
+                for(int j=0;j<4;j++)
+                {
+                    if(HexMap[signalPath[i] - '0'][j] == '0')
+                    {
+                        v->get_value_vector()->push_back(VCD_0);
+                    }else
+                    {
+                        v->get_value_vector()->push_back(VCD_1);
+                    }
+                    
+                }
+
+            }else
+            {
+                //0xabcdef
+                for(int j=0;j<4;j++)
+                {
+                    if(HexMap[(signalPath[i] - 'a') + 10][j] == '0')
+                    {
+                        v->get_value_vector()->push_back(VCD_0);
+                    }else
+                    {
+                        v->get_value_vector()->push_back(VCD_1);
+                    }
+                }
+            }
+        
+        }
+        return v;
+
+    }else if(signalPath[0] == '0' && signalPath[1] == 'X')
+    {
+        VCDValue* v = new VCDValue(new VCDBitVector());
+        for(int i=2;i<signalPath.length();i++)
+        {
+            if(signalPath[i] >= '0' && signalPath[i] <= '9')
+            {
+                //0x0123456789
+                for(int j=0;j<4;j++)
+                {
+                    if(HexMap[signalPath[i] - '0'][j] == '0')
+                    {
+                        v->get_value_vector()->push_back(VCD_0);
+                    }else
+                    {
+                        v->get_value_vector()->push_back(VCD_1);
+                    }
+                    
+                }
+
+            }else
+            {
+                //0xABCDEF
+                for(int j=0;j<4;j++)
+                {
+                    if(HexMap[(signalPath[i] - 'A') + 10][j] == '0')
+                    {
+                        v->get_value_vector()->push_back(VCD_0);
+                    }else
+                    {
+                        v->get_value_vector()->push_back(VCD_1);
+                    }
+                }
+            }
+        
+        }
+        return v;
+    }else if(signalPath[0] == '0' && signalPath[1] == 'b')
+    {
+        //binary
+        VCDValue* v = new VCDValue(new VCDBitVector());
+        for(int i=2;i<signalPath.length();i++)
+        {
+            if(signalPath[i] == '0')
+            {
+                v->get_value_vector()->push_back(VCD_0);
+            }else
+            {
+                v->get_value_vector()->push_back(VCD_1);
+            }
+            
+        
+        }
+        return v;
+
+    }else if(signalPath[0] == '0' && signalPath[1] == 'o')
+    {
+        //octonary
+        VCDValue* v = new VCDValue(new VCDBitVector());
+        for(int i=2;i<signalPath.length();i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                if(OctMap[(signalPath[i] - '0')][j] == '0')
+                {
+                    v->get_value_vector()->push_back(VCD_0);
+                }else
+                {
+                    v->get_value_vector()->push_back(VCD_1);
+                }
+                
+            }
+            
+        }
+        return v;
+    }else if(signalPath[0] >= '0' && signalPath[0] <= '9')
+    {
+        VCDValue* v = new VCDValue(new VCDBitVector());
+        int value = stoi(signalPath);
+        std::vector<int> temp_res;
+
+        while(value)
+        {
+            if(value % 2 == 1)
+            {
+                temp_res.push_back(1);
+            }else
+            {
+                temp_res.push_back(0);
+            }
+            
+            value = value / 2;
+        }
+        std::reverse(temp_res.begin(),temp_res.end());
+        for(int i=0;i<temp_res.size();i++)
+        {
+            if(temp_res[i] == 1)
+            {
+                v->get_value_vector()->push_back(VCD_1);
+            }else
+            {
+                v->get_value_vector()->push_back(VCD_0);
+            }
+            
+        }
+        return v;
+
+    }else
+    {
+        //handle identifier
+        VCDSignal* sig = trace->get_signal_by_path(signalPath);
+        assert(sig && "Signal can't be found!");
+        VCDValue* v = trace->get_signal_value_at(sig->hash, time);
+        return v;
+    }
 }
 
 void XVisitor::set_time(VCDTime t) {
